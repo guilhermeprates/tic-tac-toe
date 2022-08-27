829a4e756bb811ce2c2fa688dce94372d270429b
Guilherme Prates <contact@guilhermeprates.com>
2022-08-27 16:25:29 -0300

    Update board builder

--- Game/Board.gd
+++ Game/Board.gd
@@ -5,15 +5,15 @@ const BOARD_DIMENSIONS = Vector2(3, 3)
 
 enum PLAYER { X, O }
 
+var board
+
 func _ready():
 	build_board()
 
 func build_board():
-	for x in range(BOARD_DIMENSIONS.x):
-		for y in range(BOARD_DIMENSIONS.y):
-			print(x, y)
-			var tile = load("res://Game/BoardTile.tscn").instance()
-			tile.position = Vector2(x * 173, y * 176)
-			tile.hide()
-			add_child(tile)
-
+	board = []
+	for line in range(BOARD_DIMENSIONS.x):
+		board.append([])
+		for column in range(BOARD_DIMENSIONS.y):
+			board[line].append(column)
+	print(board)
--- Game/BoardTile.gd
+++ Game/BoardTile.gd
@@ -1,11 +1,19 @@
-extends Sprite
+extends Area2D
+class_name BoardTile
 
 signal boardtile_clicked
 
-func _input(event):
-	if event.is_action_pressed("mouse_left_click"):
-		if get_rect().has_point(to_local(event.position)):
-			if !visible:
-				show()
-				emit_signal("boardtile_clicked")
+export(int) var line
+export(int) var column
+export(Array, Texture) var player_symbol
 
+var sprite
+
+func _ready():
+	sprite = get_node("Sprite")
+	
+func _input_event(_viewport, event, _shape_idx):
+	if event.is_action_pressed("mouse_left_click"):
+		print("%d,%d" % [line, column])
+		sprite.texture = player_symbol[1];
+		emit_signal("boardtile_clicked", line, column)
