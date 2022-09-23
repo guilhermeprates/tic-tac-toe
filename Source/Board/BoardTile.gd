0873692451111dd17bccd5db4d85e6b49f543bc3
Guilherme Prates <contact@guilhermeprates.com>
2022-09-23 13:38:22 -0300

    Add click sound

--- Source/Board/BoardTile.gd
+++ Source/Board/BoardTile.gd
@@ -7,11 +7,13 @@ export(int) var column
 export(Array, Texture) var symbol_textures
 
 onready var sprite: Sprite = $Sprite
+onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 var symbol: int = 0
 
 func _input_event(_viewport: Object, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("mouse_left_click"):
+		audio_stream_player.play()
 		emit_signal("boardtile_selected", line, column)
 
 func _to_string() -> String:
